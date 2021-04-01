31f075cd1437a420c928d0f63c71e07f2869f8c6
Alejandro-Montero-Porras <alejandro.montero.porras@alumnos.upm.es>
2021-04-01 19:47:14 +0200

    inicio menu

--- Menú_Principal.c
+++ Menú_Principal.c
@@ -2,8 +2,21 @@
 
 int main ()
 {
-    switch (Elegir)
+    float d1,d2,d3, d5;
+    printf ("Seleccione modo de juego o salir.\n");
+    printf ("Para jugar a Blackjack pulse d1, para jugar a Scaperoom pulse d2 y para Salir pulse d3.\n");
+    scanf ("%f %f %f %f" , &d1, &d2, &d3, &d5);
+    switch (d5)
+{
+    case 'd1':
+    printf("Blackjack.\n");
 
-case 'B':
-    case 'b:'
+    case 'd2':
+    printf("Scaperoom.\n");
+
+    case 'd3':
+    printf("Hasta la próxima.\n");
+    break;
+}
+
 }
