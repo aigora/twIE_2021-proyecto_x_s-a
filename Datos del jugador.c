27b9b247793956bef6203691bd8cccf1a01096be
Marpiki11 <marco.gaspar@alumnos.upm.es>
2021-04-25 19:41:01 +0200

    Inicio del blackjack
    Se elige el número de participantes, pudiendo introducir sus nombres y la cantidad inicial para jugar.

--- Datos del jugador.c
+++ Datos del jugador.c
@@ -1,1 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void borrar();
+
+typedef struct
+{
+    char nombre[50];
+    double diner_inicio;
+}datos_ini;
+
+
+int main ()
+{
+    int num_jug, i;
+
+    printf("Bienvenido a Blackjack.\n");
+    system ("pause");
+
+    do
+    {
+    borrar();
+    printf("Elija un numero de jugadores de 1 a 7.\n");
+    scanf("%i",&num_jug);
+    }
+    while ((num_jug<1)||(num_jug>7));
+
+    datos_ini participante[num_jug];
+
+
+    for (i=0;i < num_jug;i++)
+    {
+    printf("Escriba el nombre del participante %i.\n",i+1);
+    scanf("%s",&participante[i].nombre);
+    printf("Elija la cantidad inicial con la que %s quiere empezar a jugar.\n",participante[i].nombre);
+    scanf("%f",&participante[i].diner_inicio);
+    }
+
+
+    return 0;
+
+}
+
+
+
+
+void borrar()
+{
+    system("cls");
+}
+
+
+
 
