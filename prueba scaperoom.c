04ae68a44902e8bc843eab0e969e8a65c051981e
Alejandro-Montero-Porras <alejandro.montero.porras@alumnos.upm.es>
2021-04-26 20:45:54 +0200

    Actualización menor scaperoom

--- prueba scaperoom.c
+++ prueba scaperoom.c
@@ -226,18 +226,31 @@ int main ()
                                 {
                                 int decision7;
                                 printf("-Empezaré por la izquierda, siendo zurdo eso tendría que darme suerte.\n");
+                                Continuar();
                                 printf("*Comienzas a buscar por la parte izquierda del almacén por algún tipo de razón supersticiosa*\n");
+                                Continuar();
                                 printf("-Aquí parece haber muchas cajas, espero tener suerte.\n");
+                                Continuar();
                                 printf(" *Estás un buen rato buscando hasta que encuentras la antena parabólica, no es tan grande como esperabas, pero servirá*\n");
+                                Continuar();
                                 printf("-Una cosa menos, solo faltan dos.\n");
+                                Continuar();
                                 printf("*Sigues buscando*\n");
+                                Continuar();
                                 printf("-Parece que aquí no hay nada más.\n");
+                                Continuar();
                                 printf("*Empiezas a buscar por la derecha*\n");
+                                Continuar();
                                 printf("*Abres la primera caja y encuentras el micrófono*\n");
+                                Continuar();
                                 printf("-Parece que he vuelto a tener suerte.\n");
+                                Continuar();
                                 printf("*Después de un rato buscando encuentras el chip y comienzas a salir del almacén*\n");
+                                Continuar();
                                 printf("-Ya tengo todo lo que buscaba y lo he conseguido bastante rápido, parece que he tenido suerte.\n");
+                                Continuar();
                                 printf("*Mientas buscabas se ha hecho de noche*\n");
+                                Continuar();
                                 printf("DECISIÓN 1: Quedarte dentro de la nave.\n");
                                 printf("DECISIÓN 2: Salir de la nave y crear una hoguera.\n");
                                 printf("Pulse cualquier otro numero para volver a leer.\n");
@@ -247,15 +260,20 @@ int main ()
                                     case 1:
                                     s=1;
                                     printf("-Me quedaré aquí dentro, es más seguro que salir.\n");
+                                    Continuar();
                                     printf("*Te tumbas encima de un montón de cajas y duermes hasta el día siguiente*\n");
+                                    Continuar();
                                     printf("*Te despiertas y escuchas un ruido parecido al de un helicóptero, sales y compruebas que es un vehículo aéreo que acaba de aterrizar. Del interior de la nave sale una criatura. ¡Es un humano! ¿Cómo es posible? Te sientas en el suelo debido a la confusión. Este te explica que pertenece al equipo de rescate de la Agencia Espacial Europea, después del lanzamiento de tu nave esta tuvo un fallo mecánico provocando que se estrellase contra el suelo. Este accidente te ha provocado pérdidas de memoria y no recuerdas nada posterior al lanzamiento. Eres llevado al hospital más cercano para que seas atendido*\n");
+                                    Continuar();
                                     printf("HAS GANADO\n");
                                     break;
 
                                     case 2:
                                     s=1;
                                     printf("-Necesito salir de esta nave, no me da seguridad, no tanta como salir.\n");
+                                    Continuar();
                                     printf("*Creas una hoguera y te duermes profundamente, al cabo de un rato parte del combustible procedente del depósito llega hasta ti. El fuego de la hoguera chisporrotea provocando un incendio al caer sobre el combustible. Mueres quemado*\n");
+                                    Continuar();
                                     printf("GAME OVER\n");
                                     break;
 
@@ -268,15 +286,25 @@ int main ()
                                 case 2:
                                 e=1;
                                 printf("Empezaré a buscar por la derecha.\n");
+                                Continuar();
                                 printf("*Comienzas la búsqueda de los materiales por la parte derecha del almacén*\n");
+                                Continuar();
                                 printf("-Parece que es mi día de suerte, en la primera caja que abro encuentro el micrófono, esto me ahorrará tiempo.\n");
+                                Continuar();
                                 printf("*Sigues buscando y encuentras el chip*\n");
+                                Continuar();
                                 printf("-¡Genial! Ahora solo me falta la antena parabólica.\n");
+                                Continuar();
                                 printf("*Detrás de unas cajas encuentras una gran antena, ya tienes todos los materiales y sigue siendo de día*\n");
+                                Continuar();
                                 printf("-Perfecto, ya solo me queda montar el radio transmisor.\n");
+                                Continuar();
                                 printf("*Te diriges al exterior del almacén para montar el transmisor cuando a lo lejos ves una figura acercándose a ti, entras en pánico y disparas con tu arma*\n");
+                                Continuar();
                                 printf("-No sé qué sería eso, pero creo que me he salvado. Voy a ver que era.\n");
+                                Continuar();
                                 printf("*Te acercas al cadáver del ser al que acabas de disparar y es un humano. No sabes que hacer, tu cerebro colapsa y caes al suelo. Despiertas dentro de un calabozo. Nunca llegaste a abandonar la Tierra, tu nave se estrelló después de un fallo mecánico y por culpa del golpe no recuerdas nada posterior al lanzamiento. Pasarás el resto de tus días en la cárcel*\n");
+                                Continuar();
                                 printf("GAME OVER\n");
                                 break;
 
