0e3d3ec1b12ac543e333cb43a3741843a09dfdd6
Marco Antonio Pinto Tapia <79769585+Marco-Antonio-Pinto@users.noreply.github.com>
2021-04-26 21:58:07 +0200

    Mejora de datos de jugador
    double --> float
    organización

--- Datos del jugador.c
+++ Datos del jugador.c
@@ -7,7 +7,7 @@ void borrar();
 typedef struct
 {
     char nombre[50];
-    double diner_inicio;
+    float monedero;
 }datos_ini;
 
 
@@ -20,30 +20,25 @@ int main ()
 
     do
     {
-    borrar();
-    printf("Elija un numero de jugadores de 1 a 7.\n");
-    scanf("%i",&num_jug);
-    }
-    while ((num_jug<1)||(num_jug>7));
+        borrar();
+        printf("Elija un numero de jugadores de 1 a 7.\n");
+        scanf("%i",&num_jug);
+    }while ((num_jug<1)||(num_jug>7));
 
     datos_ini participante[num_jug];
 
-
-    for (i=0;i < num_jug;i++)
+    for (i=0; i < num_jug;i++)
     {
-    printf("Escriba el nombre del participante %i.\n",i+1);
-    scanf("%s",&participante[i].nombre);
-    printf("Elija la cantidad inicial con la que %s quiere empezar a jugar.\n",participante[i].nombre);
-    scanf("%f",&participante[i].diner_inicio);
+        printf("Escriba el nombre del participante %i.\n",i+1);
+        scanf("%s", &participante[i].nombre);
+        do
+        {
+            printf("Elija la cantidad inicial con la que %s quiere empezar a jugar.\n",participante[i].nombre);
+            scanf("%f", &participante[i].monedero);
+        }while((participante[i].monedero <= 0));
     }
-
-
     return 0;
-
 }
-
-
-
 
 void borrar()
 {
