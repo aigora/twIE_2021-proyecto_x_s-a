21a53fd36590a34d5942297b655c1e22429adf55
Marco Antonio Pinto Tapia <79769585+Marco-Antonio-Pinto@users.noreply.github.com>
2021-05-03 21:32:17 +0200

    Mejora del Programa
    Implementación del color, Y principio del Black Jack

--- Programa generador de cartas.c
+++ Programa generador de cartas.c
@@ -2,7 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <windows.h>
+
+/// Definimos variables
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
 
 ///Funciones
 void Borrar();
@@ -16,7 +25,7 @@ typedef struct
 
 typedef struct
 {
-    e_carta carta;
+    e_carta carta[2];
     char nombre[100];
     int monedero;
 }jugador;
@@ -28,7 +37,7 @@ int main()
 
     char carta[] = {'A', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'J', 'Q', 'K', '\0'};
     char palo[] = { 3, 4, 5, 6, '\0' };
-    int n_c, i;
+    int n_c, i, j;
     char nc, pc;
     int num_jug, i1;
     srand(time(NULL));
@@ -42,8 +51,7 @@ int main()
         scanf("%i",&num_jug);
     }while ((num_jug<1)||(num_jug>7));
 
-    jugador participante[num_jug]
-    ;
+    jugador participante[num_jug];
 
     for (i1=0; i1 < num_jug;i1++)
     {
@@ -53,9 +61,9 @@ int main()
         {
             printf("Elija la cantidad inicial con la que %s quiere empezar a jugar.\n",participante[i].nombre);
             scanf("%i", &participante[i1].monedero);
-        }while((participante[i1].monedero <=0));        //NO funciona si se introduce un número decimal negativo y entre 0 y 1
-                                                        //usar if para cambiar el valor negativo a positivo.
-                                                        for(i = 0; i < 2; i++)
+        }while((participante[i1].monedero <=0));
+
+        for(i = 0; i < 2; i++)
         {
             nc = rand()%12;
             pc = rand()%4;
@@ -64,16 +72,19 @@ int main()
                 if ((palo[pc] == 3)||(palo[pc] == 4))
                 {
                     n_c = 10;
-                    printf("\033[0;31m");
-                    printf("%d %c \n", n_c, palo[pc]);
-                    printf("\033[0;37m");
+                    printf(ANSI_COLOR_RED);
+                    printf("%d%c", n_c, palo[pc]);
+                    printf(ANSI_COLOR_RESET);
+                    participante[i1].carta[i].numero = n_c;
+                    participante[i1].carta[i].palo = palo[pc];
                 }
                 if ((palo[pc] == 5)||(palo[pc] == 6))
                 {
                     n_c = 10;
-                    printf("\033[0;34m");
-                    printf("%d %c \n", n_c, palo[pc]);
-                    printf("\033[0;37m");
+                    printf("%d%c", n_c, palo[pc]);
+                    printf(ANSI_COLOR_RESET);
+                    participante[i1].carta[i].numero = n_c;
+                    participante[i1].carta[i].palo = palo[pc];
                 }
 
             }
@@ -81,18 +92,60 @@ int main()
             {
                 if ((palo[pc] == 3)||(palo[pc] == 4))
                 {
-                    printf("\033[0;31m");
-                    printf("%c %c \n", carta[nc], palo[pc]);
-                    printf("\033[0;37m");
+                    printf(ANSI_COLOR_RED);
+                    printf("%c%c ", carta[nc], palo[pc]);
+                    printf(ANSI_COLOR_RESET);
+                    participante[i1].carta[i].numero = carta[nc];
+                    participante[i1].carta[i].palo = palo[pc];
                 }
                 if ((palo[pc] == 5)||(palo[pc] == 6))
                 {
-                    printf("\033[0;34m");
-                    printf("%c %c \n", carta[nc], palo[pc]);
-                    printf("\033[0;37m");
+                    printf("%c%c ", carta[nc], palo[pc]);
+                    printf(ANSI_COLOR_RESET);
+                    participante[i1].carta[i].numero = carta[nc];
+                    participante[i1].carta[i].palo = palo[pc];
                 }
             }
         }
+        printf("\n");
+    }
+    int x, y;
+    for(x = 0; x < num_jug; x++)
+    {
+        printf("Las cartas de %s son:", participante[x].nombre);
+        for(y = 0; y < 2; y++)
+        {
+            if(participante[x].carta[y].numero == 10)
+            {
+                if((participante[x].carta[y].palo == 3)||(participante[x].carta[y].palo == 4))
+                {
+                    printf(ANSI_COLOR_RED);
+                    printf("%i%c ", participante[x].carta[y].numero, participante[x].carta[y].palo);
+                    printf(ANSI_COLOR_RESET);
+                }
+                if((participante[x].carta[y].palo == 5)||(participante[x].carta[y].palo == 6))
+                {
+                    printf("%i%c ", participante[x].carta[y].numero, participante[x].carta[y].palo);
+                    printf(ANSI_COLOR_RESET);
+                }
+            }
+            else
+            {
+                if((participante[x].carta[y].palo == 3)||(participante[x].carta[y].palo == 4))
+                {
+                    printf(ANSI_COLOR_RED);
+                    printf("%c%c ", participante[x].carta[y].numero, participante[x].carta[y].palo);
+                    printf(ANSI_COLOR_RESET);
+                }
+                if((participante[x].carta[y].palo == 5)||(participante[x].carta[y].palo == 6))
+                {
+                    printf("%c%c ", participante[x].carta[y].numero, participante[x].carta[y].palo);
+                    printf(ANSI_COLOR_RESET);
+                }
+            }
+
+        }
+        printf("\n");
     }
     return 0;
 }
